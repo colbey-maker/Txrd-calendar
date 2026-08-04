24421134099fcf44146f3886fe7611bd8b2d42fa
colbey-maker <colbey@workmans.us>
2026-08-04 10:54:53 -0500

    Add initial calendar builder

---
+++ scripts/build.py
@@ -0,0 +1,20 @@
+"""
+TXRD Calendar Builder
+Version 1.0
+"""
+
+from pathlib import Path
+
+print("🛼 TXRD Calendar Builder")
+print()
+
+# Project folders
+ROOT = Path(__file__).parent.parent
+DATA = ROOT / "data"
+
+print("Project folder:")
+print(ROOT)
+
+print()
+print("Data folder:")
+print(DATA)
